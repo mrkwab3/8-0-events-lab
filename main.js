004eb3809910b45afa67f525fcdb86294dea7edb
mrkwab3 <mrkwab3@gmail.com>
2023-01-14 19:36:11 -0500

    Added a current color selector

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+let paletteColors = document.querySelectorAll('#palette, .color')
+let currentColor = document.querySelector('#current-color')
+const cells = document.querySelectorAll('.cell')
+
+// Within the colors in the palette, if a color div is clicked, the current colors background will be changed to the color of the div clicked
+for (color of paletteColors) {
+  color.addEventListener('click', (event) => {
+    currentColor.style.background = event.target.style.background
+  })
+}
+
+
+
+
+
+
