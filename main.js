6f6daf240ba295263c8c451b9ca3a8494ba3abfc
mrkwab3 <mrkwab3@gmail.com>
2023-01-14 20:50:28 -0500

    Completed clicking interaction and buttons

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ for (let i = 0; i < 100; i++) {
 
 let paletteColors = document.querySelectorAll('#palette, .color')
 let currentColor = document.querySelector('#current-color')
-const cells = document.querySelectorAll('.cell')
+let cells = document.querySelectorAll('.cell')
 
 // Within the colors in the palette, if a color div is clicked, the current colors background will be changed to the color of the div clicked
 for (color of paletteColors) {
@@ -19,7 +19,37 @@ for (color of paletteColors) {
   })
 }
 
+// for (cell of cells) {
+//   cell.addEventListener('click', () => {
+//     cell.style.background = currentColor.style.background
+//   })
+// }
 
+// Why does for Each work but not a 'for of' loop?
+// For each cell, if the cell is clicked its background color will switch to the color of the current color
+cells.forEach((cell) => {
+  cell.addEventListener('click', () => {
+    cell.style.background = currentColor.style.background
+  })
+})
+
+// When the button is clicked, all cells will change to the current color
+const changeAll = document.querySelector('.changeALL')
+
+changeAll.addEventListener('click', () => {
+  cells.forEach((cell) => {
+    cell.style.background = currentColor.style.background
+  })
+})
+
+// When the button is clicked, all cells will be reset to their default color
+const resetAll = document.querySelector('.resetALL')
+
+resetAll.addEventListener('click', () => {
+  cells.forEach((cell) => {
+    cell.style.background = 'white'
+  })
+})
 
 
 
